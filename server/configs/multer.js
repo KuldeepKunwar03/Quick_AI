c75bffed0bca74d9f211665b9993a5a3531c0191
KuldeepKunwar03 <kuldeepkunwar16@gmail.com>
2026-09-21 01:08:04 +0530

    removed backend

--- server/configs/multer.js
+++
@@ -1,7 +0,0 @@
-import multer from "multer";
-
-// Files are only held long enough to forward to Cloudinary / the PDF parser,
-// then unlinked by the controller.
-const upload = multer({ storage: multer.diskStorage({}) })
-
-export default upload
